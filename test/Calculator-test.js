e88d922ca275c72c3587425bb7cdc30f54d995e4
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-05-20 20:57:36 +0200

    creation

---
+++ test/Calculator-test.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+const { ethers } = require("ethers");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Calculator");
+    const greeter = await Greeter.deploy();
+    await greeter.deployed();
+    expect(await greeter.nbOperation()).to.equal(0);
+  });
+});
